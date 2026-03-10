e2437109d445f5f28cc753a83d45e431bc11577a
Vincenzo Dallapé <dallapevichen12@gmail.com>
2026-03-10 16:08:28 -0300

    fix

--- CRM/src/components/ranking/PodiumCard.tsx
+++ CRM/src/components/ranking/PodiumCard.tsx
@@ -66,7 +66,7 @@ export function PodiumCard({ ranking, position }: PodiumCardProps) {
         <Icon className="h-5 w-5 text-white" />
       </div>
 
-      {/* Avatar */}
+      {/* Avatar1 */}
       <div className="relative mb-3">
         {avatarUrl ? (
           <img
